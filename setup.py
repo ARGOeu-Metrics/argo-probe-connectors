717997ba21581d18eba72732c976164185c7a123
Daniel Vrcic <daniel.vrcic@gmail.com>
2023-01-25 18:09:00 +0100

    initial distutils files

---
+++ setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+import glob
+import sys
+
+NAME='argo-probe-connectors'
+NAGIOSPLUGINS='/usr/libexec/argo/probes/connectors'
+
+def get_ver():
+    try:
+        for line in open(NAME+'.spec'):
+            if "Version:" in line:
+                return line.split()[1]
+    except IOError:
+        sys.exit(1)
+
+
+setup(name=NAME,
+      version=get_ver(),
+      license='ASL 2.0',
+      author='SRCE, GRNET',
+      author_email='dvrcic@srce.hr, kzailac@srce.hr',
+      description='Package includes probe for checking argo-connectors component',
+      platforms='noarch',
+      url='http://argoeu.github.io/',
+      data_files=[(NAGIOSPLUGINS, glob.glob('src/*'))],
+      packages=['argo_probe_connectors'],
+      package_dir={'argo_probe_connectors': 'modules/'},
+      )
